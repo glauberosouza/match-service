e0a336ec0829300483cde34c87dd3c18eea2f623
glaub <glaubero.souza@gmail.com>
2023-11-28 15:20:06 -0300

    Bugfix

--- src/main/java/com/glauber/MatchService/configuration/mailConfiguration.java
+++ src/main/java/com/glauber/MatchService/configuration/mailConfiguration.java
@@ -13,11 +13,11 @@ public class mailConfiguration {
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         // Estou utilizando o https://mailtrap.io/signin
-        mailSender.setHost("yourHost");
+        mailSender.setHost("sandbox.smtp.mailtrap.io");
         mailSender.setPort(2525);
 
-        mailSender.setUsername("YourUser");
-        mailSender.setPassword("YourEmail");
+        mailSender.setUsername("bc4bfed446613a");
+        mailSender.setPassword("f38b9be7a3f1d0");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
